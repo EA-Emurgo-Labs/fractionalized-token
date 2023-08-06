e958ada1a9cb11b52bc188db71c13edae088aacd
thinhnguyen <thinhnq@blockchainlabs.asia>
2023-08-06 14:29:09 +0000

    format test

--- tests/ContractTest.hs
+++ tests/ContractTest.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE DataKinds          #-}
 {-# LANGUAGE FlexibleInstances  #-}
 {-# LANGUAGE NoImplicitPrelude  #-}
-
 {-# OPTIONS_GHC -Wno-orphans #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Redundant bracket" #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
+{-# HLINT ignore "Redundant bracket" #-}
 module Main where
 
 import           Data.ByteString           (count)
@@ -43,27 +42,27 @@ import           Wallet                    (payToPaymentPublicKeyHash)
 
 ---------------------------------------------------------------------------------------------------
 --------------------------------------- TESTING MAIN ----------------------------------------------
-
 -- | Test the validator script
 main :: IO ()
-main = defaultMain $
-    testGroup
-  "Testing script properties"
-  [
-    testGroup "Fractionalized NFT contract"
-      [ testProperty "Check mint fnft " prop_mint_fnft
-      ]
-  ]
+main =
+  defaultMain $
+  testGroup
+    "Testing script properties"
+    [ testGroup
+        "Fractionalized NFT contract"
+        [testProperty "Check mint fnft " prop_mint_fnft]
+    ]
 
 -- ---------------------------------------------------------------------------------------------------
 -- -------------------------------- HELPER FUNCTIONS/INSTANCES ---------------------------------------
-
 -- Init a new blockchain with 10,000,000 ADA and 1 Emurgo token (the operator token).
 mock :: Mock
-mock = initMock defaultBabbage (adaValue 10_000_000 <> emurgoValue)
+mock = initMock defaultBabbage (adaValue 10 _000_000 <> emurgoValue)
 
 instance Testable a => Testable (Run a) where
-  property rp = let (a,_) = runMock rp mock in property a
+  property rp =
+    let (a, _) = runMock rp mock
+     in property a
 
 -- Convert from String to BuiltinByteString
 toBuiltinByteString :: String -> BuiltinByteString
@@ -85,7 +84,7 @@ emurgoValue = fakeValue fake 1000
 -- Set up users, including: one operator and one normal user.
 setupUsers :: Run [PubKeyHash]
 setupUsers = do
-  issuer    <- newUser (adaValue 1000 <> emurgoValue)
+  issuer <- newUser (adaValue 1000 <> emurgoValue)
   buyer <- newUser (adaValue 1000)
   pure [issuer, buyer]
 
@@ -102,74 +101,97 @@ policyMintingContractScript = TypedPolicy $ toV2 Main.getMintingPolicy
 
 -- ---------------------------------------------------------------------------------------------------
 -- ------------------------------------- TESTING PROPERTIES ------------------------------------------
-
 prop_mint_fnft :: Property
 prop_mint_fnft = runChecks
 
 -- ---------------------------------------------------------------------------------------------------
 -- ------------------------------------- RUNNING THE TESTS -------------------------------------------
-lockTx :: UserSpend -> TxOutRef -> Value -> Value -> Value -> GeneralParams.FNFTDatum -> Tx
-lockTx usp ref valNFT fracVal validationVal datum = mconcat
-  [ userSpend usp
-  , mintValue policyMintingContractScript (InitialMint ref) (fracVal <> validationVal)
-  , payToScript fnftContract (HashDatum datum) (valNFT <> fracVal <> validationVal)
-  ]
+lockTx ::
+     UserSpend
+  -> TxOutRef
+  -> Value
+  -> Value
+  -> Value
+  -> GeneralParams.FNFTDatum
+  -> Tx
+lockTx usp ref valNFT fracVal validationVal datum =
+  mconcat
+    [ userSpend usp
+    , mintValue
+        policyMintingContractScript
+        (InitialMint ref)
+        (fracVal <> validationVal)
+    , payToScript
+        fnftContract
+        (HashDatum datum)
+        (valNFT <> fracVal <> validationVal)
+    ]
 
-unlockTx :: PubKeyHash -> TxOutRef -> Value -> Value -> Value -> GeneralParams.FNFTDatum -> Tx
-unlockTx receiver ref valNFT fracVal validationVal datum = mconcat
-  [ spendScript fnftContract ref () datum
-  , mintValue policyMintingContractScript Burn (fracVal <> validationVal)
-  , payToKey receiver (valNFT)
-  ]
+unlockTx ::
+     PubKeyHash
+  -> TxOutRef
+  -> Value
+  -> Value
+  -> Value
+  -> GeneralParams.FNFTDatum
+  -> Tx
+unlockTx receiver ref valNFT fracVal validationVal datum =
+  mconcat
+    [ spendScript fnftContract ref () datum
+    , mintValue policyMintingContractScript Burn (fracVal <> validationVal)
+    , payToKey receiver (valNFT)
+    ]
 
 runChecks :: Property
-runChecks =
-  monadic property check
+runChecks = monadic property check
   -- monadic property check
-    where
-      check = do
-        isGood <- run $ testValues
-        assert isGood
+  where
+    check = do
+      isGood <- run $ testValues
+      assert isGood
 
 testValues :: Run Bool
 testValues = do
-    [issuer, buyer] <- setupUsers
-    utxos <- utxoAt issuer
-    let nftVal  = fakeValue fake 10
-        [(ref, out)] = utxos
-        fracTN = TokenName $ calculateFractionTokenNameHash ref
-        validationTN = TokenName $ fromString "FNFT_VALIDITY"
-        fracVal = singleton MintingContract.mintingContractSymbol fracTN 1000
-        validationVal = singleton MintingContract.mintingContractSymbol validationTN 1
-
-    uspIssuer <- spend issuer nftVal
-    let fnftDatum = GeneralParams.FNFTDatum {
-        GeneralParams.fractionAC = assetClass MintingContract.mintingContractSymbol fracTN
-        , GeneralParams.emittedFractions = 1000
-    }
-    let tx  = lockTx uspIssuer ref nftVal fracVal  validationVal fnftDatum
-    submitTx issuer tx
-
-    Plutus.Model.waitUntil 50
-
-    let fracValBurn = singleton MintingContract.mintingContractSymbol fracTN (-1000)
-        validationValBurn = singleton MintingContract.mintingContractSymbol validationTN (-1)
-
-    burnUtxos <- utxoAt fnftContract
-    let nft = find(\x -> do
-            let (txOutRef', txOut') = x
-                value' = txOutValue txOut'
-                flatValues = flattenValue value'
-            case find(\(cs, tn, amt) -> cs == MintingContract.mintingContractSymbol) flatValues of
-                  Nothing -> False
-                  Just _  -> True
-            ) burnUtxos
-
-    let (txOutRef, txOut) = fromJust nft
-
-    let txBurn  = unlockTx issuer txOutRef nftVal fracValBurn validationValBurn fnftDatum
-    submitTx issuer txBurn
-    Plutus.Model.waitUntil 50
-    noErrors
-
-
+  [issuer, buyer] <- setupUsers
+  utxos <- utxoAt issuer
+  let nftVal = fakeValue fake 10
+      [(ref, out)] = utxos
+      fracTN = TokenName $ calculateFractionTokenNameHash ref
+      validationTN = TokenName $ fromString "FNFT_VALIDITY"
+      fracVal = singleton MintingContract.mintingContractSymbol fracTN 1000
+      validationVal =
+        singleton MintingContract.mintingContractSymbol validationTN 1
+  uspIssuer <- spend issuer nftVal
+  let fnftDatum =
+        GeneralParams.FNFTDatum
+          { GeneralParams.fractionAC =
+              assetClass MintingContract.mintingContractSymbol fracTN
+          , GeneralParams.emittedFractions = 1000
+          }
+  let tx = lockTx uspIssuer ref nftVal fracVal validationVal fnftDatum
+  submitTx issuer tx
+  Plutus.Model.waitUntil 50
+  let fracValBurn =
+        singleton MintingContract.mintingContractSymbol fracTN (-1000)
+      validationValBurn =
+        singleton MintingContract.mintingContractSymbol validationTN (-1)
+  burnUtxos <- utxoAt fnftContract
+  let nft =
+        find
+          (\x -> do
+             let (txOutRef', txOut') = x
+                 value' = txOutValue txOut'
+                 flatValues = flattenValue value'
+             case find
+                    (\(cs, tn, amt) ->
+                       cs == MintingContract.mintingContractSymbol)
+                    flatValues of
+               Nothing -> False
+               Just _  -> True)
+          burnUtxos
+  let (txOutRef, txOut) = fromJust nft
+  let txBurn =
+        unlockTx issuer txOutRef nftVal fracValBurn validationValBurn fnftDatum
+  submitTx issuer txBurn
+  Plutus.Model.waitUntil 50
+  noErrors
